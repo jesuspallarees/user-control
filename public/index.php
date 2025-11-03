b2b2192fcc92380171e61c98845d03fb6c8774d7
jesuspallarees <jespaldia@gmail.com>
2025-11-03 15:56:37 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'web.php';
---
+++ resources/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+if (!isset($_SERVER['REQUEST_URI'])) {
+    $peticion = '/';
+} else {
+    $peticion = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+}
+
+
+$lista_blanca = [
+    '/' => 'home.php',
+];
+
+$estructura_carpetas = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+if (array_key_exists($peticion, $lista_blanca)) {
+    require $estructura_carpetas . $lista_blanca[$peticion];
+} else {
+    http_response_code(404);
+    require $estructura_carpetas . '404.php';
+}
